24f70cee27de37a30ddfde4b52bb6119e4184bd8
d1sc0 <ilovediscostu@gmail.com>
2019-11-29 00:21:52 +0000

    update title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Disco GatBoot`,
+    title: `Disco Gats Boot`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `Stuart Mackenzie`,
   },
